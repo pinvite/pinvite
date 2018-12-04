4d60ecda27f50228c0a68bae18112460caeb5977
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-05 06:24:26 +0900

    Add buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,8 @@ import {Header1} from '../components/style-typography'
 import Layout from '../components/layout'
 import {withAuthStatusContext} from '../context/HOC'
 import {LayoutBLockCenter} from '../components/styled'
-import { navigateTo } from 'gatsby-link';
 
 class IndexPage extends React.Component {
-
-  componentDidMount() {
-    if (this.props.context.result) navigateTo('/invite/')
-  }
-
-  componentWillReceiveProps (nextProps) {
-    if (nextProps.context.result) navigateTo('/invite/')
-  }
 
   render () {
     console.log(this.props.context)
