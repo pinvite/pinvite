b661d9b8bd5e9be4dc7c8effecf2d39ea9526808
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-08 18:28:39 +0900

    Introduce index page's specific template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,13 @@
 import React from 'react'
-import {ButtonTwitter} from '../components/Atoms/Button'
 import ApplicationBar from '../components/Molecules/ApplicationBar'
-import Layout from '../components/Templates/Layout'
-import InviteSamplePaper from '../components/Molecules/InviteSamplePaper'
-import CallToAction from '../components/Molecules/CallToAction'
-import IssueListPaper from '../components/Molecules/IssueListPaper'
+import Index from '../components/Templates/Index'
 import {withAuthStatusContext} from '../context/HOC'
 
-class IndexPage extends React.Component {
-  render () {
-    return (
-      <Layout>
-        <ApplicationBar />
-        <CallToAction
-          description='Twitterで勉強会の講師を募集しましょう'
-          buttonText='Twitterで登録'
-        />
-        <InviteSamplePaper />
-        <IssueListPaper />
-        <CallToAction
-          description='勉強会を開きましょう！pinviteがTwitterでの講師探しをお手伝いします' 
-          buttonText='Twitterで登録'
-        />
-      </Layout>
-    )
-  }
-}
+const IndexPage = () =>
+  <Index
+    firstCallToActionText='Twitterで勉強会の講師を募集しましょう'
+    secondCallToActionText='Twitterで勉強会の講師を募集しましょう'
+    buttonText='Twitterで登録'
+  />
 
 export default withAuthStatusContext(IndexPage)
