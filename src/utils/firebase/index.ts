7933b033b063a34ffd1c0ace52d71a5296890b33
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-10 20:16:59 +0900

    Firebase in ts

---
+++ src/utils/firebase/index.ts
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyB0wAwkW0DAfyKT7bkhLuQ3IeMPD_ykNUQ',
+  authDomain: 'pinvite-app.firebaseapp.com',
+  databaseURL: 'https://pinvite-app.firebaseio.com',
+  projectId: 'pinvite-app',
+  // storageBucket: "pinvite-app.appspot.com",
+  // messagingSenderId: "474917508837"
+})
+
+export const providerGoogle = new firebase.auth.GoogleAuthProvider()
+export const providerFacebook = new firebase.auth.FacebookAuthProvider()
+export const providerTwitter = new firebase.auth.TwitterAuthProvider()
+export const firestore = firebase.firestore() // firestroeを使う場合
+// Disable deprecated features
+firestore.settings({
+  timestampsInSnapshots: true,
+})
+
+export default firebase
