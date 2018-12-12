912b30100583b401112bcd9ac449142a86ef95b4
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-12 23:34:46 +0900

    Firebase login flow with async await

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -20,4 +20,76 @@ firestore.settings({
   timestampsInSnapshots: true,
 })
 
+interface TwitterScreenName {
+  screen_name: string
+}
+
+interface TwitterCredential {
+  access_token: string,
+  secret: string,
+}
+
+type TwitterUserInfo = TwitterCredential & TwitterScreenName
+
+function isTwitterScreenName(obj: any): obj is TwitterScreenName {
+  return (obj as TwitterScreenName).screen_name !== undefined
+}
+
+function isTwitterCredential(obj: any): obj is TwitterCredential {
+  const credential = obj as TwitterCredential
+  return credential.access_token !== undefined && credential.secret !== undefined
+}
+
+function isTwitterUserInfo(obj: any): obj is TwitterUserInfo {
+  return isTwitterScreenName(obj) && isTwitterCredential(obj)
+}
+
+export async function login(): Promise<TwitterUserInfo> {
+  try {
+    const userCredentials = await firebase.auth().signInWithPopup(providerTwitter)
+    const firebaseUser = userCredentials.user
+    if (firebaseUser == null) {
+      return Promise.reject(new Error('Failed to get user info upon login'))
+    } else {
+      const userDoc = await firestore.collection('users').doc(firebaseUser.uid).get()
+      const data = userDoc.data()
+      // data() returns 'undefined' if the document doesn't exist.
+      if (data !== undefined
+        && data.twitter
+        && isTwitterUserInfo(data.twitter)
+      ) {
+        return Promise.resolve({
+          access_token: data.twitter.access_token,
+          screen_name: data.twitter.screen_name,
+          secret: data.secret,
+        })
+
+      // if data is not stored in Firestore, then try storing it
+      } else {
+        if (
+          userCredentials.credential == null ||
+          // According to https://firebase.google.com/docs/auth/web/twitter-login
+          // userCredentials.credential should have `accessToken` and `secret` properties
+          !isTwitterCredential(userCredentials.credential) ||
+          userCredentials.additionalUserInfo == null ||
+          userCredentials.additionalUserInfo.profile == null ||
+          !isTwitterScreenName(userCredentials.additionalUserInfo.profile)
+        ) {
+          return Promise.reject(new Error('Failed to get necessary Twitter info via Sign-In'))
+        } else {
+          const twitterUserInfo: TwitterUserInfo = {
+            access_token: userCredentials.credential.access_token,
+            screen_name: userCredentials.additionalUserInfo.profile.screen_name,
+            secret: userCredentials.credential.secret,
+          }
+          await firestore.collection('users').doc(firebaseUser.uid).set({twitter: twitterUserInfo})
+          return Promise.resolve(twitterUserInfo)
+        }
+      }
+    }
+  } catch (error) {
+    return Promise.reject(new Error('Internal server error upon login'))
+  }
+}
+
 export default firebase
