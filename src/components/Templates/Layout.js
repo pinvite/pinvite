264c5cb7914e6fc90d1ba4f89854f80a5310ff57
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-15 01:32:38 +0900

    Simplify Layout.js

--- src/components/Templates/Layout.js
+++ src/components/Templates/Layout.js
@@ -1,52 +1,30 @@
 import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
-const Wrapper = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  overflow: scroll;
-`
-
 const Container = styled.div`
-  max-width: 480px;
+  max-width: 600px;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
 `
 
 const Layout = ({ children }) => {
   return (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Fragment>
-        <Helmet>
-          <title>pinvite</title>
-          <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        </Helmet>
-        <Wrapper>
-          <Container>
-            {children}
-          </Container>
-        </Wrapper>
-      </Fragment>
-    )}
-  />
-)}
+    <Fragment>
+      <Helmet>
+        <title>pinvite</title>
+        <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </Helmet>
+      <Container>
+        {children}
+      </Container>
+    </Fragment>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
