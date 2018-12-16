adf4946f8aa5400bfb682edfe9d934e2b16b6385
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-16 15:24:45 +0900

    Jump to invite page when logged in

--- src/context/AuthStatusContext.tsx
+++ src/context/AuthStatusContext.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import firebase, { firebaseLogin, makeSureTwitterUserInfoStored, UserInfo } from '../utils/firebase'
 
-enum LoginStatus {
+export enum LoginStatus {
   NotLoggedIn,
   LoggedIn,
   ReadyToTweet,
