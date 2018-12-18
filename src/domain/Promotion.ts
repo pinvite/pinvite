33d6545183bce4a9c293248eaaa99b250a9bdf15
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-18 09:17:35 +0900

    init domain objs (#144)

---
+++ src/domain/Promotion.ts
@@ -0,0 +1,7 @@
+export interface WorkshopPromotion {
+  readonly title: string
+  readonly details: string
+  readonly time: number
+  readonly moneyAmount: number
+  readonly imageURL: string
+}
---
+++ src/domain/Twitter.ts
@@ -0,0 +1,27 @@
+import {WorkshopPromotion} from './Promotion'
+
+export interface TwitterCard {
+  readonly twitterCard: string
+  readonly twitterSite: string
+  readonly twitterCreater: string
+  readonly ogURL: string
+  readonly ogTitle: string,
+  readonly ogDescription: string,
+  readonly ogImage: string
+}
+
+export function toTwitterCard(
+  promotion: WorkshopPromotion,
+  twitterUserName: string,
+  pageURL: string
+) {
+  return ({
+    twitterCard: 'summary_large_image',
+    twitterSite: '@orgpinvite',
+    twitterCreater: '@' + twitterUserName,
+    ogURL: pageURL,
+    ogTitle: promotion.title,
+    ogDescription: promotion.details,
+    ogImage: promotion.imageURL,
+  })
+}
