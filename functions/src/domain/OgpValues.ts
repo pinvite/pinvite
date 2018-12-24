06d291e246bbd94807078e9e40d4f2b9529fc7f2
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-24 17:51:22 +0900

    Copy domain objs to Firebase Functions (#158)
    * wip
    
    * Copy domain objs to Firebase Functions

---
+++ functions/src/domain/OgpValues.ts
@@ -0,0 +1,30 @@
+// DO NOT edit this directly in functions/src/domain. (i.e. Firebase Functions directory)
+// Edit only this in src/domain (i.e. Firebase hosting directory) then copy it to functions/src/domain by `npm run build`
+
+import {WorkshopPromotion} from './Promotion'
+
+export interface OgpValues {
+  readonly twitterCard: string
+  readonly twitterSite: string
+  readonly twitterCreater: string
+  readonly ogURL: string
+  readonly ogTitle: string,
+  readonly ogDescription: string,
+  readonly ogImage: string
+}
+
+export function toOgpValues(
+  promotion: WorkshopPromotion,
+  pageURL: string
+): OgpValues {
+  return ({
+    twitterCard: promotion.twitterCard,
+    twitterSite: promotion.twitterSite,
+    twitterCreater: '@' + promotion.twitterUserId,
+    ogURL: pageURL,
+    ogTitle: promotion.title,
+    ogDescription: promotion.details,
+    ogImage: promotion.imageURL,
+  })
+}
+  
