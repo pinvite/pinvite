1eba4cb7baf972a1f489987ca432c11dc927b017
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-27 03:49:21 +0900

    Refactoring 2 (#166)

--- functions/src/domain/Invitation.ts
+++ functions/src/domain/Invitation.ts
@@ -27,23 +27,3 @@ export function isInvitationInfo(obj: any): obj is InvitationInfo {
   )
 }
 
-// It is an HTTP request body structure, not actually a domain object though... 
-export interface InvitationRequest {
-  readonly title: string
-  readonly details: string
-  readonly time: number
-  readonly moneyAmount: number
-  readonly imageURL: string
-}
-
-export function isInvitationRequest(obj: any): obj is InvitationRequest {
-  // User-defined Type Guards at https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards
-  const promotion = obj as InvitationRequest
-  return (
-    promotion.title !== undefined
-    && promotion.details !== undefined
-    && promotion.time !== undefined
-    && promotion.moneyAmount !== undefined
-    && promotion.imageURL !== undefined
-  )
-}
