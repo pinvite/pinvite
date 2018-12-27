fe218f6c481efd06e8a98efd8c7f6daefeb0a571
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-12-27 17:32:35 +0900

    Fix OGP tag population on SSR

--- src/components/Atoms/OgpMetaTags.tsx
+++ src/components/Atoms/OgpMetaTags.tsx
@@ -5,14 +5,14 @@ import {OgpValues} from '../../domain/OgpValues'
 const OgpMetaTags: React.SFC<OgpValues> = (props) =>
   <Helmet
     meta={[
-      { name: 'twitter:card',    content: props.twitterCard },
-      { name: 'twitter:site',    content: props.twitterSite },
-      { name: 'twitter:creator', content: props.twitterCreator },
-      { name: 'og:url',          content: props.ogURL },
-      { name: 'og:title',        content: props.ogTitle },
-      { name: 'og:description',  content: props.ogDescription },
-      { name: 'og:image',        content: props.ogImage },
-      { name: 'title',           content: props.ogTitle }
+      { name:     'twitter:card',    content: props.twitterCard },
+      { name:     'twitter:site',    content: props.twitterSite },
+      { name:     'twitter:creator', content: props.twitterCreator },
+      { property: 'og:url',          content: props.ogURL },
+      { property: 'og:title',        content: props.ogTitle },
+      { property: 'og:description',  content: props.ogDescription },
+      { property: 'og:image',        content: props.ogImage },
+      { name:     'title',           content: props.ogTitle }
     ]}
   />
 
