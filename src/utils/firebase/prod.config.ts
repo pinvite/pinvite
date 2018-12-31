017fa5f463932eca03c36b2fe125d409aa3c8a84
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-01-01 01:04:36 +0900

    Cypress and CircleCI deployment (#181)
    * Initialize cypress
    
    * Use tsconfig.json instead of triple-slash comment
    
    * Remove extra files
    
    * Add the staging project
    
    * Deploy job in CircleCI
    
    * fix package-lock.json
    
    * Fix circleci config
    
    * Cache node_modules
    
    * Use token in Firebase use as well
    
    * Cache doesn't seem to accelerate the build
    
    * Smart way to switch Firebase api keys

---
+++ src/utils/firebase/prod.config.ts
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: 'AIzaSyB0wAwkW0DAfyKT7bkhLuQ3IeMPD_ykNUQ',
+  authDomain: 'pinvite-app.firebaseapp.com',
+  databaseURL: 'https://pinvite-app.firebaseio.com',
+  projectId: 'pinvite-app',
+}
+
+export default config
