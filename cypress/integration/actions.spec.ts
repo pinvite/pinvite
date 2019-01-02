cd22ef3e0025cc927ce13305597e6c6dd95d7d24
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-01-02 21:48:29 +0900

    Add login func in Cypress

--- cypress/integration/actions.spec.ts
+++ cypress/integration/actions.spec.ts
@@ -1,15 +1,32 @@
 /// <reference types="cypress" />
 
+import {login} from "../support/login"
+
 describe('The main page', function() {
-  describe('When not logged in', () => {
-    describe('The primary button(s)', () => {
-      it('should show the text to encourage logging-in', () => {
-        if (process.env.TEST_HOST === undefined) {
-          cy.visit('http://localhost:8000')
-        } else {
-          cy.visit(process.env.TEST_HOST)
-        }
-    
+  beforeEach(function() {
+    if (Cypress.env('TEST_HOST') === undefined) {
+      cy.visit('http://localhost:8000')
+    } else {
+      cy.visit(Cypress.env('TEST_HOST'))
+    }
+  })
+
+  describe('When NOT logged in', function() {
+    describe('The primary button(s)', function() {
+      it('should show the text to encourage logging-in', function() {
+        cy.get('[data-cy=primary-button]')
+          .should('contain', 'Twitterで登録')
+        })
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function(){
+      login()
+    })
+
+    describe('The primary button(s)', function() {
+      it('should show the text to move to the entry page', function() {
         cy.get('[data-cy=primary-button]')
           .should('contain', 'Twitterで登録')
         })
