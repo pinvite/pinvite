09581d6535cf5101a3aa3cb513e83c320f5b3f85
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-01-03 23:46:57 +0900

    Add profile picture upon login (#194)

--- src/components/Molecules/ApplicationBar.tsx
+++ src/components/Molecules/ApplicationBar.tsx
@@ -1,21 +1,56 @@
-import AppBar from '@material-ui/core/AppBar'
 import { navigate } from 'gatsby'
 import React from 'react'
+import styled from 'styled-components'
+import AppBar from '@material-ui/core/AppBar'
+import { AuthStatusContext, LoginStatus } from '../../context/AuthStatusContext'
 import AppName from '../Atoms/AppName'
+import ProfilePicture, { ProfilePictureProps } from '../Atoms/ProfilePicture'
 
 export interface ApplicationBarProps {
   className?: string // allow styled-components to inject CSS margin from outside. Only margin, no other CSS property from outside.
 }
 
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ProfilePictureStyled = styled(ProfilePicture as React.SFC<ProfilePictureProps>)`
+  && {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+`
+
 const ApplicationBar: React.SFC<ApplicationBarProps> = props => (
   // Important to accept the className prop, to inject CSS margin from outside.
   // Only margin, no other CSS property from outside.
   <AppBar className={props.className} color="secondary" position="relative">
-    <AppName
-      onClick={() => {
-        navigate('/')
+    <AuthStatusContext.Consumer>
+      {({ loginStatus, userInfo }) => {
+        if (loginStatus === LoginStatus.LoggedIn && userInfo ) {
+          return (
+            <FlexContainer>
+              <AppName
+                onClick={() => {
+                  navigate('/')
+                }}
+              />
+              <ProfilePictureStyled photoURL={userInfo.photoURL} />
+            </FlexContainer>
+          )
+        } else {
+          return (
+            <AppName
+              onClick={() => {
+                navigate('/')
+              }}
+            />
+          )
+        }
       }}
-    />
+    </AuthStatusContext.Consumer>
   </AppBar>
 )
 
