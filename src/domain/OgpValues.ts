88d4952636d147272a7583cf8d0fe47bc5c65610
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-01-04 03:30:10 +0900

    Add xss package

--- src/domain/OgpValues.ts
+++ src/domain/OgpValues.ts
@@ -1,6 +1,7 @@
 // DO NOT edit this directly in functions/src/domain. (i.e. Firebase Functions directory)
 // Edit only this in src/domain (i.e. Firebase hosting directory) then copy it to functions/src/domain by `npm run build`
 
+import xss from 'xss'
 import { InvitationInfo } from './Invitation'
 
 // Twitter Cards and Open Graph at https://developer.twitter.com/en/docs/tweets/optimize-with-cards/guides/getting-started.html
@@ -18,14 +19,14 @@ export interface OgpValues {
 
 export function toOgpValues(invitationInfo: InvitationInfo): OgpValues {
   return {
-    twitterCard: invitationInfo.twitterCard,
-    twitterSite: '@' + invitationInfo.twitterSiteOwnerId,
-    twitterCreator: '@' + invitationInfo.twitterUserId,
-    ogURL: invitationInfo.pageURL,
-    ogTitle: invitationInfo.title,
-    ogDescription: invitationInfo.details,
-    ogImage: invitationInfo.imageURL,
-    title: invitationInfo.title,
+    twitterCard: xss(invitationInfo.twitterCard),
+    twitterSite: '@' + xss(invitationInfo.twitterSiteOwnerId),
+    twitterCreator: '@' + xss(invitationInfo.twitterUserId),
+    ogURL: xss(invitationInfo.pageURL),
+    ogTitle: xss(invitationInfo.title),
+    ogDescription: xss(invitationInfo.details),
+    ogImage: xss(invitationInfo.imageURL),
+    title: xss(invitationInfo.title),
   }
 }
 
