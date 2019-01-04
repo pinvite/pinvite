0be7ef2cb900589f4b27d1b90d2e6262fd1c90db
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-01-05 01:26:19 +0900

    URL needs to be URL in meta tag

--- src/domain/OgpValues.ts
+++ src/domain/OgpValues.ts
@@ -23,6 +23,8 @@ const REGEXP_DOUBLE_QUOTE = /"/g
 const REGEXP_SINGLE_QUOTE = /'/g
 const REGEXP_SLASH = /\//g
 
+// https://stackoverflow.com/questions/13246540/html-and-attribute-encoding#answer-33811648
+// https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet#Output%20Encoding%20Rules%20Summary
 function sanitize(untrustedAttribute: string): string {
   return untrustedAttribute
     .replace(REGEXP_AMPERSAND, '&amp;')
@@ -33,15 +35,36 @@ function sanitize(untrustedAttribute: string): string {
     .replace(REGEXP_SLASH, '&#x2F;')
 }
 
+// https://security.stackexchange.com/questions/11985/will-javascript-be-executed-which-is-in-an-href#answer-11989
+// > <a href="javascript:alert(8007)">Click me</a>
+// As in the above SO answer, complex attribute like href is more vulnerable as it can execute JavaScript.
+// However, meta tag's content attribute does not execute JavaScript, so following this advice:
+//   https://stackoverflow.com/questions/13246540/html-and-attribute-encoding#answer-33811648
+//   > Properly quoted attributes can only be escaped with the corresponding quote.
+//   https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet#RULE_.232_-_Attribute_Escape_Before_Inserting_Untrusted_Data_into_HTML_Common_Attributes
+//   > This should not be used for complex attributes like href, src, style, or any of the event handlers like onmouseover. It is extremely important that event handler attributes should follow Rule #3 for HTML JavaScript Data Values.
+// we are sanitizing only a smaller set of characters for URL
+function sanitizeQuotes(urlAttribute: string): string {
+  return (
+    urlAttribute
+      // .replace(REGEXP_AMPERSAND, '&amp;') & is valid in URL
+      .replace(REGEXP_LT, '&lt;')
+      .replace(REGEXP_GT, '&gt;')
+      .replace(REGEXP_DOUBLE_QUOTE, '&quot;')
+      .replace(REGEXP_SINGLE_QUOTE, '&#x27;')
+  )
+  // .replace(REGEXP_SLASH, '&#x2F;') slash is valid in URL
+}
+
 export function toOgpValues(invitationInfo: InvitationInfo): OgpValues {
   return {
     twitterCard: sanitize(invitationInfo.twitterCard),
     twitterSite: '@' + sanitize(invitationInfo.twitterSiteOwnerId),
     twitterCreator: '@' + sanitize(invitationInfo.twitterUserId),
-    ogURL: sanitize(invitationInfo.pageURL),
+    ogURL: sanitizeQuotes(invitationInfo.pageURL),
     ogTitle: sanitize(invitationInfo.title),
     ogDescription: sanitize(invitationInfo.details),
-    ogImage: sanitize(invitationInfo.imageURL),
+    ogImage: sanitizeQuotes(invitationInfo.imageURL),
     title: sanitize(invitationInfo.title),
   }
 }
