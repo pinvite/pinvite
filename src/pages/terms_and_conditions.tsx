14242e9c2721044a9b43ed97a45f6a5d02ed1ace
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-01-12 21:49:24 +0900

    Terms and Conditions page (#216)

---
+++ src/pages/terms_and_conditions.tsx
@@ -0,0 +1,6 @@
+import React, { Fragment } from 'react'
+import TermsAndConditions from '../components/Templates/TermsAndConditions'
+
+const TermsAndConditionsPage = () => <TermsAndConditions />
+
+export default TermsAndConditionsPage
